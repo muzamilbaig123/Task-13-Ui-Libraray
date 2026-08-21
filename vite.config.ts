643a19293124517e0199ab13b41775fecf02ee13
muzamilbaig123 <muzamilbaig24@gmail.com>
2026-08-21 11:54:21 +0500

    fix: app build mode for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,30 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "EaseUI",
-      fileName: (format) => `easeui.${format}.js`,
-    },
-    cssCodeSplit: true,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+  ...(mode === "library" && {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "EaseUI",
+        fileName: (format) => `easeui.${format}.js`,
+      },
+      cssCodeSplit: true,
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
       },
     },
-  },
-});
+  }),
+}));
